be9cc1232b24b1a551393a5d7730b250c031d503
jh <>
2024-11-21 20:01:02 -0500

    got a messy celery setup working

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from PIL.Image import Image
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 import fastapi
@@ -6,13 +7,47 @@ from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 
 import schemas as _schemas
-import services as _services
+import services
 import io
+
+from celery import Celery
+from celery.result import AsyncResult
+
+# from kombu.serialization import register
+# import pydanticserializer
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="../frontend/build")
 app.mount('/static', StaticFiles(directory="../frontend/build/static"), 'static')
+
+# # register(
+# #     "pydantic",
+# #     pydanticserializer.pydantic_dumps,
+# #     pydanticserializer.pydantic_loads,
+# #     content_type="application/x-pydantic",
+# #     content_encoding="utf-8",
+# # )
+
+# celery = Celery(
+#     'tasks',
+#     broker='redis://localhost:6379/0',
+#     backend='redis://localhost:6379/0',
+# )
+
+# # celery.conf.update(
+# #     task_serializer="pydantic",
+# #     result_serializer="pydantic",
+# #     event_serializer="pydantic",
+# #     accept_content=["application/json", "application/x-pydantic"],
+# #     result_accept_content=["application/json", "application/x-pydantic"],
+# # )
+# celery.conf.update(
+#     event_serializer='pickle',
+#     task_serializer='pickle',
+#     result_serializer='pickle',
+#     accept_content = ['application/json', 'application/x-python-serialize'],
+# )
 
 @app.get('/api/health')
 async def health():
@@ -23,16 +58,49 @@ async def health():
 async def root():
     return {"message": "Hello there"}
 
-@app.get("/api/generate/")
-async def generate_image(imgPromptCreate: _schemas.ImageCreate = fastapi.Depends()):
-    print('called /api/generate endpoint', imgPromptCreate)
-    image = await _services.generate_image(imgPrompt=imgPromptCreate)
+# @app.get("/api/generate/")
+# async def generate_image(imgPromptCreate: _schemas.ImageCreate = fastapi.Depends()):
+#     print('called /api/generate endpoint', imgPromptCreate)
+#     image = await services.generate_image(imgPrompt=imgPromptCreate)
 
-    memory_stream = io.BytesIO()
-    image.save(memory_stream, format="PNG")
-    memory_stream.seek(0)
-    return StreamingResponse(memory_stream, media_type="image/png")
+#     memory_stream = io.BytesIO()
+#     image.save(memory_stream, format="PNG")
+#     memory_stream.seek(0)
+#     return StreamingResponse(memory_stream, media_type="image/png")
 
+
+
+
+
+@app.get("/api/generate2/")
+async def generate_image2(imgPromptCreate: _schemas.ImageCreate = fastapi.Depends()):
+    print('called /api/generate2 endpoint', imgPromptCreate)
+    result = services.celery.send_task('services.generate_image_task', args=[imgPromptCreate])
+    return {"message": "Task submitted", "task_id": result.id}
+
+
+
+@app.get("/task_status/{task_id}")
+async def task_status(task_id: str):
+    result = AsyncResult(task_id, app=services.celery)
+    if result.ready():
+        return {"status": "completed"}
+    else:
+        return {"status": "pending"}
+
+@app.get("/image/{task_id}")
+async def get_image(task_id: str):
+    result = AsyncResult(task_id, app=services.celery)
+    if result.ready():
+        print('result is ready')
+        image = result.get()
+
+        memory_stream = io.BytesIO()
+        image.save(memory_stream, format="PNG")
+        memory_stream.seek(0)
+        return StreamingResponse(memory_stream, media_type="image/png")
+    else:
+        return {"status": "pending"}
 
 # Serve the react app's static files
 @app.get("/{rest_of_path:path}")
--- backend/services.py
+++ backend/services.py
@@ -7,36 +7,97 @@ from PIL.Image import Image
 import os
 from dotenv import load_dotenv
 
+from celery import shared_task, Celery, Task
+
+celery = Celery(
+    'tasks',
+    broker='redis://localhost:6379/0',
+    backend='redis://localhost:6379/0',
+)
+celery.conf.update(
+    event_serializer='pickle',
+    task_serializer='pickle',
+    result_serializer='pickle',
+    accept_content = ['application/json', 'application/x-python-serialize'],
+)
+
+
+
 load_dotenv()
 HF_TOKEN = os.getenv('HF_TOKEN')
 print('hf token', HF_TOKEN)
 
-if torch.cuda.is_available():
-    device = "cuda"
-elif torch.backends.mps.is_available():
-    device = "mps"
-else:
-    device = "cpu"
+class GenerateImageTask(Task):
+    def __init__(self):
+        super().__init__()
+        self.pipe = None
+
+    def __call__(self, *args, **kwargs):
+        if not self.pipe:
+
+            if torch.cuda.is_available():
+                device = "cuda"
+            elif torch.backends.mps.is_available():
+                device = "mps"
+            else:
+                device = "cpu"
+
+
+            if device == 'cpu' or device == 'gpu':
+                pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+            else:
+                pipe = StableDiffusion3Pipeline.from_pretrained(
+                    "stabilityai/stable-diffusion-3.5-medium",
+                    torch_dtype=torch.bfloat16,
+                )
+
+
+            pipe.to(device)
+            self.pipe = pipe
+
+        return self.run(*args, **kwargs)
+
+if celery.current_worker_task:
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
 
 
 
-if device == 'cpu':
-    pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
-else:
-    pipe = StableDiffusion3Pipeline.from_pretrained(
-        "stabilityai/stable-diffusion-3.5-medium",
-        torch_dtype=torch.bfloat16,
-    )
+    if device == 'cpu' or device == 'gpu':
+        pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+    else:
+        pipe = StableDiffusion3Pipeline.from_pretrained(
+            "stabilityai/stable-diffusion-3.5-medium",
+            torch_dtype=torch.bfloat16,
+        )
 
 
 
-pipe.to(device)
+    pipe.to(device)
 
 
 async def generate_image(imgPrompt: schemas.ImageCreate) -> Image:
     generator = None if imgPrompt.seed is None else torch.Generator().manual_seed(int(imgPrompt.seed))
 
     image: Image = pipe(
+        prompt=imgPrompt.prompt,
+        negative_prompt=imgPrompt.negative_prompt,
+        guidance_scale=imgPrompt.guidance_scale,
+        num_inference_steps=imgPrompt.num_inference_steps,
+        generator = generator,
+    ).images[0]
+
+    return image
+
+@shared_task(bind=True, base = GenerateImageTask)
+def generate_image_task(self, imgPrompt: schemas.ImageCreate) -> Image:
+    generator = None if imgPrompt.seed is None else torch.Generator().manual_seed(int(imgPrompt.seed))
+
+    image: Image = self.pipe(
         prompt=imgPrompt.prompt,
         negative_prompt=imgPrompt.negative_prompt,
         guidance_scale=imgPrompt.guidance_scale,
